1b216e04e76a2f04af9add9d0f3d05b26bddcf72
reflexguru <trianglestudioad@gmail.com>
2021-12-08 03:35:17 +0300

    Integrate Discord Game SDK (without support for Windows)

---
+++ Scripts/Tools/DiscordRPCSetting.gd
@@ -0,0 +1,28 @@
+extends Node
+
+export var connect_app_id: String
+export var set_state: String
+export var set_details: String
+export var set_big_image_key: String
+export var set_big_image_text: String
+export var set_timestamp: bool
+
+func _ready() -> void:
+  call_deferred('configure_rpc')
+
+func configure_rpc() -> void:
+  if !DiscordManager.core:
+    print('[DiscordManager] Creating a new core')
+    var res = DiscordManager.create_core(int(connect_app_id))
+    if !res: return
+  else:
+    print('[DiscordManager] Using an existing core')
+    
+  var activity: = Discord.Activity.new()
+  if set_state:          activity.state = set_state
+  if set_details:        activity.details = set_details
+  if set_timestamp:      activity.timestamps.start = OS.get_unix_time()
+  if set_big_image_key:  activity.assets.large_image = set_big_image_key
+  if set_big_image_text: activity.assets.large_text = set_big_image_text
+
+  DiscordManager.activities.update_activity(activity)
---
+++ Scripts/Tools/discord_manager.gd
@@ -0,0 +1,63 @@
+# taken from the demo scripts
+
+extends Node
+
+signal initialised
+
+var core: Discord.Core
+var users: Discord.UserManager
+var images: Discord.ImageManager
+var activities: Discord.ActivityManager
+var relationships: Discord.RelationshipManager
+var networking: Discord.NetworkManager
+
+func _ready() -> void:
+  call_deferred("create_core")
+
+func _process(_delta: float) -> void:
+  if core:
+    var result: int = core.run_callbacks()
+    if result != Discord.Result.OK:
+      print("Failed to run callbacks: ", enum_to_string(Discord.Result, result))
+      destroy_core()
+
+
+func _log_hook(level: int, message: String) -> void:
+  print("[DISCORD] ", enum_to_string(Discord.LogLevel, level), ": ", message)
+
+
+func create_core(app_id) -> bool:
+  destroy_core()
+  core = Discord.Core.new()
+  var result: int = core.create(app_id, Discord.CreateFlags.DEFAULT)
+
+  if result != Discord.Result.OK:
+    print("Failed to initialise Discord Core: ", enum_to_string(Discord.Result, result))
+    destroy_core()
+    return false
+
+  core.set_log_hook(Discord.LogLevel.DEBUG, self, "_log_hook")
+
+#  users = core.get_user_manager()
+#  images = core.get_image_manager()
+  activities = core.get_activity_manager()
+#  relationships = core.get_relationship_manager()
+#  networking = core.get_network_manager()
+
+  emit_signal("initialised")
+  return true
+
+
+func destroy_core() -> void:
+  core = null
+  users = null
+  images = null
+  activities = null
+  relationships = null
+  networking = null
+
+
+func enum_to_string(the_enum: Dictionary, value: int) -> String:
+  var index := the_enum.values().find(value)
+  var string: String = the_enum.keys()[index]
+  return string
