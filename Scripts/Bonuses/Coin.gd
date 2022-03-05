1eae33513a719de6c57d67a1d8a9c63e7b1a3727
reflexguru <trianglestudioad@gmail.com>
2022-03-05 15:59:05 +0300

    Fix coin AGAIN

--- Scripts/Bonuses/Coin.gd
+++ Scripts/Bonuses/Coin.gd
@@ -12,21 +12,23 @@ func _ready() -> void:
 
 func _process(_delta) -> void:
   var g_overlaps = $BlockDetector.get_overlapping_bodies()
-  var brick:StaticBody2D
-  if g_overlaps[0] is StaticBody2D:
+  var brick: StaticBody2D
+  if (
+    len(g_overlaps) > 0 and
+    g_overlaps[0] is StaticBody2D and
+    'triggered' in g_overlaps[0] and
+    't_counter' in g_overlaps[0]
+  ):
     brick = g_overlaps[0]
     if !brick.is_in_group('Breakable'):
       return
-  else:
-    return
-    
-  if brick.triggered and brick.t_counter < 12:
-    Global.add_coins(1)
-    Global.add_score(200)
-    Global.HUD.get_node('CoinSound').play()
-    var coin_effect = CoinEffect.new(position, rotation)
-    get_parent().add_child(coin_effect)
-    queue_free()
+    if brick.triggered and brick.t_counter < 12:
+      Global.add_coins(1)
+      Global.add_score(200)
+      Global.HUD.get_node('CoinSound').play()
+      var coin_effect = CoinEffect.new(position, rotation)
+      get_parent().add_child(coin_effect)
+      queue_free()
 
 func _on_Coin_area_entered(area) -> void:
   if area.is_in_group('Mario') and not appearing:
