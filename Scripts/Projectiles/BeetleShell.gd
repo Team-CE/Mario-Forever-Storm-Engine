a0163b15fd7a90ffa06f2af465cf18efa7666b91
reflexguru <trianglestudioad@gmail.com>
2022-08-11 03:40:08 +0300

    fix delta time issues in beetle shell projectile

--- Scripts/Projectiles/BeetleShell.gd
+++ Scripts/Projectiles/BeetleShell.gd
@@ -12,13 +12,13 @@ func _ready():
   pass
 
 func _process(delta):
-  collision = move_and_collide(velocity)
+  collision = move_and_collide(velocity * Global.get_delta(delta))
   if collision and 'collider' in collision:
     if collision.collider.has_method('hit'):
       collision.collider.hit(true, false)
-      move_shell()
+      move_shell(delta)
     else:
-      move_shell()
+      move_shell(delta)
   
   var overlaps = $CollisionArea.get_overlapping_bodies()
 
@@ -29,7 +29,7 @@ func _process(delta):
         
   if belongs != 0:
     if Global.is_mario_collide('BottomDetector', self) and Global.Mario.velocity.y > 0:
-      stop_shell()
+      stop_shell(delta)
       Global.current_scene.add_child(ScoreText.new(100, global_position))
       Global.play_base_sound('ENEMY_Stomp')
       if Input.is_action_pressed('mario_jump'):
@@ -42,7 +42,7 @@ func _process(delta):
   velocity.y += 0.33 * Global.get_delta(delta)
   $AnimatedSprite.rotation_degrees += 13 * Global.get_delta(delta)
 
-func move_shell() -> void:
+func move_shell(delta) -> void:
   var shell = shell_scene.instance()
   shell.global_position = global_position
   get_parent().add_child(shell)
@@ -50,7 +50,7 @@ func move_shell() -> void:
   shell.get_node('Brain').to_moving_shell(false)
   shell.animated_sprite.speed_scale = 0.8
   shell.get_node('Brain').kill_exceptions.append(parent)
-  shell.position.y += 15
+  shell.position.y += 15 * Global.get_delta(delta)
   if Global.Mario.position.x > shell.position.x:
     shell.dir *= -1
   
@@ -59,12 +59,12 @@ func move_shell() -> void:
   
   call_deferred('queue_free')
 
-func stop_shell() -> void:
+func stop_shell(delta) -> void:
   var shell = shell_scene.instance()
   shell.global_position = global_position
   get_parent().add_child(shell)
   shell.vars = shell.vars.duplicate(false)
   shell.get_node('Brain').to_stopped_shell()
-  shell.position.y += 15
+  shell.position.y += 15 * Global.get_delta(delta)
   
   call_deferred('queue_free')
